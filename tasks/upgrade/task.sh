c5fc05b1a921bb51d51334057e67c564ad54bca6
František Nečas <fnecas@redhat.com>
2022-05-24 12:35:50 +0200

    Specify reboot command to tmt-reboot
    Signed-off-by: František Nečas <fnecas@redhat.com>

--- tasks/upgrade/task.sh
+++ tasks/upgrade/task.sh
@@ -9,14 +9,18 @@ rlJournalStart
     rlPhaseEnd
 
     rlPhaseStartTest "Update"
-        rlRun "rpm -qa | sort | tee old" 0 "Check packages before update"
-        rlRun "dnf --refresh update -y" 0 "Update to the latest packages"
-        rlRun "dnf upgrade -y" 0 "Update to the latest packages"
-        rlRun "dnf install dnf-plugin-system-upgrade -y" 0 "Install dnf upgrade plugin"
-        rlRun "dnf system-upgrade download --releasever=$TARGET -y" 0 "Download new Fedora packages"
-        rlRun "dnf system-upgrade reboot" 0 "start actual upgrade"
-        rlRun "rpm -qa | sort | tee new" 0 "Check packages after update"
-        rlRun "diff old new" 1 "Compare package lists"
+        if [[ "$TMT_REBOOT_COUNT" == "0" ]]; then
+            rlRun "rpm -qa | sort | tee old" 0 "Check packages before update"
+            rlRun "dnf --refresh update -y" 0 "Update to the latest packages"
+            rlRun "dnf upgrade -y" 0 "Update to the latest packages"
+            rlRun "dnf install dnf-plugin-system-upgrade -y" 0 "Install dnf upgrade plugin"
+            rlRun "dnf system-upgrade download --releasever=$TARGET -y" 0 "Download new Fedora packages"
+            rlRun "tmt-reboot -c \"dnf system-upgrade reboot\"" 0 "Start actual upgrade"
+        else
+            rlLog "Successfully rebooted"
+            rlRun "rpm -qa | sort | tee new" 0 "Check packages after update"
+            rlRun "diff old new" 1 "Compare package lists"
+        fi
     rlPhaseEnd
 
     rlPhaseStartCleanup
